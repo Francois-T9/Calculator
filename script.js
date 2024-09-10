18a35b56ffc1da5292853702473918fb0015a8d6
Francois <francois.thullier98@gmail.com>
2024-09-10 14:05:43 +0200

    The calculator works and display the result in the screen, but multiple operators are not supported yet

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let a;
-let b;
-let operator;
 
 function add(a,b) {
     return(a+b);
@@ -42,6 +39,70 @@ function displayCopy(str) {
 
 }
 
+let displayValue=document.querySelector(".display");
+let evalButton=document.querySelector(".evalBtn");
+
+
+evalButton.addEventListener("click",function() {
+    // console.log("you pressed eval button");
+    // operate()
+})
+
+let screenValue='';
+
+let a;
+let b;
+let operator;
+
+let strOperator="+-*/";
+
+function separateStr(str) {
+    
+    for(let i=0;i<str.length;i++) {
+        
+        if(str[i]=="+" || str[i]=="-" || str[i]=="*" || str[i]=="/") {
+
+            a=str.slice(0,i);
+            operator=str[i];
+            b=str.slice(i+1,str.length);
+            // console.log(operator);
+            return([a,b,operator]);
+
+        }
+    }
+
+
+    // takes for example "12+1" and return a=12, b=1 and operand ="+"
+}
+
+// console.log(separateStr("124+12"));
+
 document.querySelectorAll("button").forEach(button=> {
     button.addEventListener("click",function() {
-        displayCopy(this.textContent)})});
+        
+        if(this.textContent=="C") {
+            screenValue="";
+            displayCopy(screenValue);
+
+        }
+        else if(this.textContent=="=") {
+            // console.log(displayValue.textContent);
+            a=separateStr(displayValue.textContent)[0];
+            b=separateStr(displayValue.textContent)[1];
+            
+            operator=separateStr(displayValue.textContent)[2];
+            let resu=operate(parseInt(a),parseInt(b),operator);
+            console.log(resu);
+            displayCopy(resu);
+            
+        }
+        else {
+            
+            screenValue+=this.textContent;
+            displayCopy(screenValue);
+        }
+        
+        
+        
+    })});
+
